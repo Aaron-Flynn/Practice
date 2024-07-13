27c2e9039ffd7f6aaa00700b8ca05269d64aa2de
Aaron Flynn <aaronflynn866@gmail.com>
2024-07-13 13:30:49 +0000

    Add

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,4 +1,4 @@
-const buttons = document.getElementsById("control");
+const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementsById("player-score");
 const ComputerScore = document.getElementsById("computer-score");
 const playerImage = document.getElementsById("player-image");
@@ -6,15 +6,39 @@ const ComputerImage = document.getElementsById("computer-image");
 const messages = document.getElementsById("messages");
 const choices = ["rock", "paper", "scissors"];
 
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
-    })};
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
 
-    
+  for (let button of buttons) {
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "submit") {
+              alert("You clicked Submit!");
+          } else {
+              let gameType = this.getAttribute("data-type");
+              alert(`You clicked ${gameType}`);
+          }
+      });
+  }
+
+});
 
 
+function playGame(playerChoice) {
+playerImage.src = `assets/images/${choices[playerChoice]}.png;
+playerImage.alt = choices[playerChoice];
+
+let computerChoice = Math.floor(Math.random() * 3);
+
+ComputerImage.src = `assets/images/${choices[computerChoice]}.png ;
+ComputerImage.alt = choices[computerChoice];
+
+let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+updateScores(result);
+
+}
+
+/*
 const getUseerChoice = userInput => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
